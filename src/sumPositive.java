abb564109fad8751092eb1925014567a8cb1112a
orinmed25 <orin20002510@gmail.com>
2025-11-25 23:11:35 +0200

    Added more tests and improve main function

--- src/sumPositive.java
+++ src/sumPositive.java
@@ -1,6 +1,9 @@
 public class sumPositive {
-    public static int sumPositive(int arr[], int size)
+    public static int sum(int arr[], int size)
     {
+        if (arr == null) {
+            throw new NullPointerException("Array cannot be null");
+        }
         int sum = 0;
         for(int i = 0; i < size; i++)
         {
--- src/sumPositiveTest.java
+++ src/sumPositiveTest.java
@@ -2,9 +2,73 @@ import org.junit.*;
 
 public class sumPositiveTest {
     @Test
-    public void test(){
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int size1 = 5;
-        Assert.assertEquals(15, sumPositive.sumPositive(arr1, size1));
+    public void testAllPositive() {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        Assert.assertEquals(15, sumPositive.sum(arr, 5));
+    }
+
+    @Test
+    public void testAllNegative() {
+        int[] arr = { -1, -2, -3, -4, -5 };
+        Assert.assertEquals(0, sumPositive.sum(arr, 5));
+    }
+
+    @Test
+    public void testSinglePositive() {
+        int[] arr = { 5 };
+        Assert.assertEquals(5, sumPositive.sum(arr, 1));
+    }
+
+    @Test
+    public void testSingleNegative() {
+        int[] arr = { -3 };
+        Assert.assertEquals(5, sumPositive.sum(arr, 1));
+    }
+
+    @Test
+    public void testPositiveAndNegative() {
+        int[] arr = { 2, -1, 3, -4 };
+        Assert.assertEquals(5, sumPositive.sum(arr, 4));
+    }
+
+    @Test
+    public void testPartialArray() {
+        int[] arr = { 1, 2, 3, 4 };
+        Assert.assertEquals(6, sumPositive.sum(arr, 3));
+    }
+
+    @Test
+    public void testEmptyArray() {
+        int[] arr = {};
+        Assert.assertEquals(0, sumPositive.sum(arr, 0));
+    }
+
+    @Test
+    public void testWithZeros() {
+        int[] arr = { 1, 0, 2, 0, 3 };
+        Assert.assertEquals(6, sumPositive.sum(arr, 5));
+    }
+
+    @Test
+    public void testAllZeros() {
+        int[] arr = { 0, 0, 0 };
+        Assert.assertEquals(0, sumPositive.sum(arr, 3));
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testSizeExceedsArrayLength() {
+        int[] arr = { 1, 2, 3 };
+        sumPositive.sum(arr, 5);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testNullArray() {
+        sumPositive.sum(null, 0);
+    }
+
+    @Test
+    public void testNegativeSize() {
+        int[] arr = { 1, 2, 3 };
+        Assert.assertEquals(0, sumPositive.sum(arr, -1));
     }
 }
